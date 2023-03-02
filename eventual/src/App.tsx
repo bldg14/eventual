5870320c1f37ac3e8ab1b8d2a38774276a895fdc
eduenez33 <duenezedwin33@gmail.com>
2023-03-02 07:24:23 -0600

    Create Event component
    Create Event component that takes title and description props as type of string. https://github.com/bldg14/eventual/issues/7

--- eventual/src/App.tsx
+++ eventual/src/App.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 import './App.css';
 import Heading from './heading/heading'
 
+function Event({title, description}: {title: string; description: string;}) {
+  return (
+    <div>
+      <Heading title={ title } />
+      <p>
+        { description }
+      </p>
+    </div>
+  );
+}
+
 class App extends React.Component {
 
   render() {
@@ -10,6 +21,7 @@ class App extends React.Component {
         <header className="App-header">
           <Heading title="Eventual" />
         </header>
+        <Event title="Bike Ride" description="Nice ride along the coast" />
       </div>
     );
   }
