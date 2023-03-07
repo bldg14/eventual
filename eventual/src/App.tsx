ddf476c04c22129ed82660825f1caf7c02a73073
eduenez33 <duenezedwin33@gmail.com>
2023-03-07 08:02:53 -0600

    Import Event Component
    Remove Event Component and import it from Event directory

--- eventual/src/App.tsx
+++ eventual/src/App.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import './App.css';
 import Heading from './heading/heading'
-
-function Event({title, description}: {title: string; description: string;}) {
-  return (
-    <div>
-      <Heading title={ title } />
-      <p>
-        { description }
-      </p>
-    </div>
-  );
-}
+import Event from './event/event'
 
 function App() {
     return (
