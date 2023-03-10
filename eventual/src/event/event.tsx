748a127994764dc2d734a9ad2cd93960cc79266c
eduenez33 <duenezedwin33@gmail.com>
2023-03-09 18:13:18 -0600

    Change to arrow function syntax

--- eventual/src/event/event.tsx
+++ eventual/src/event/event.tsx
@@ -1,13 +1,11 @@
 import Heading from '../heading/heading'
 
-function Event({title, description}: {title: string; description: string;}) {
-    return (
-      <div>
-        <Heading title={ title } />
-        <p>
-          { description }
-        </p>
-      </div>
-    );
-  }
+  const Event = ({ title, description }: { title: string; description: string; }) => 
+  <div>
+    <Heading title={ title } />
+    <p>
+      { description }
+    </p>
+  </div>
+
 export default Event;
