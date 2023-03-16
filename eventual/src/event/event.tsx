a81e1b110f61f186e607adf41d8594e2c81658bc
eduenez33 <duenezedwin33@gmail.com>
2023-03-16 18:39:10 -0500

    Reformat Event component

--- eventual/src/event/event.tsx
+++ eventual/src/event/event.tsx
@@ -1,11 +1,18 @@
-import Heading from '../heading/heading'
+import Heading from "../heading/heading";
 
-  const Event = ({ title, description }: { title: string; description: string; }) => 
-  <div>
-    <Heading title={ title } />
-    <p>
-      { description }
-    </p>
-  </div>
+const Event = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div>
+      <Heading title={title} />
+      <p>{description}</p>
+    </div>
+  );
+};
 
 export default Event;
