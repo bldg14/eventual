8050cd990394c8052e852062fec7b361ba1ed433
eduenez33 <duenezedwin33@gmail.com>
2023-04-02 15:09:52 -0500

    Add rest of event props

--- src/event/event.tsx
+++ src/event/event.tsx
@@ -1,16 +1,30 @@
-import Heading from "../heading/heading";
-
-const Event = ({
-  title,
-  description,
-}: {
+interface EventProps {
   title: string;
   description: string;
-}) => {
+  start: Date;
+  end: Date;
+  url: string;
+  email: string;
+}
+
+const Event = ({ title, description, start, end, url, email }: EventProps) => {
   return (
-    <div>
-      <Heading title={title} />
-      <p>{description}</p>
+    <div className="event-container">
+      <h2 className="event-title">{title}</h2>
+      <div className="event-details">
+        <div className="event-time-container">
+          <p className="event-time">
+            {start.toLocaleTimeString()} - {end.toLocaleTimeString()}
+          </p>
+        </div>
+        <div className="event-description-container">
+          <p className="event-description">{description}</p>
+        </div>
+      </div>
+      <div className="event-footer">
+        <button className="event-url">{url}</button>
+        <button className="event-email">{email}</button>
+      </div>
     </div>
   );
 };
--- src/event/events.test.tsx
+++ src/event/events.test.tsx
@@ -6,7 +6,16 @@ test("renders the Event component", () => {
   const title = "Event";
   const description = "This is the Event Component";
 
-  render(<Event title={title} description={description} />);
+  render(
+    <Event
+      title={title}
+      description={description}
+      start={new Date()}
+      end={new Date()}
+      url="https://www.google.com"
+      email="hello@world.com"
+    />
+  );
 
   const elTitle = screen.getByText(title);
   const elDesc = screen.getByText(description);
