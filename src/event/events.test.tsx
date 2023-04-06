9291ce6239974740bd5f10a64ee8f5f2046a294f
eduenez33 <duenezedwin33@gmail.com>
2023-04-06 18:49:09 -0500

    Update Event test to reflect all props

--- src/event/events.test.tsx
+++ src/event/events.test.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Event from "./event";
 
-test("renders the Event component", () => {
-  const title = "Event";
-  const description = "This is the Event Component";
+describe("Event", () => {
+  const props = {
+    title: "Test Title",
+    description: "Test Description",
+    start: new Date("2023-04-05T14:30:00Z"),
+    end: new Date("2023-04-05T16:30:00Z"),
+    url: "https://example.com",
+    email: "test@example.com",
+  };
 
-  render(
-    <Event
-      title={title}
-      description={description}
-      start={new Date()}
-      end={new Date()}
-      url="https://www.google.com"
-      email="hello@world.com"
-    />
-  );
+  it("renders the component with correct props", () => {
+    render(<Event {...props} />);
 
-  const elTitle = screen.getByText(title);
-  const elDesc = screen.getByText(description);
-  expect(elTitle).toBeInTheDocument();
-  expect(elDesc).toBeInTheDocument();
+    expect(screen.getByText(props.title)).toBeInTheDocument();
+    expect(screen.getByText(props.description)).toBeInTheDocument();
+    expect(screen.getByText("9:30 AM - 11:30 AM")).toBeInTheDocument();
+    expect(screen.getByText(props.url)).toBeInTheDocument();
+    expect(screen.getByText(props.email)).toBeInTheDocument();
+  });
 });
