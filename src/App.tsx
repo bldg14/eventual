3cf07031fff8197bca6e22846cf2b434529d9d4c
eduenez33 <duenezedwin33@gmail.com>
2023-07-20 18:57:48 -0500

    Add Col component with attributes to center

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,23 @@ function App() {
   return (
     <Container>
       <Row>
-        <header>
-          <h1>Eventual</h1>
-        </header>
+        <Col md={{ span: 6, offset: 3 }} className="text-center">
+          <header>
+            <h1>Eventual</h1>
+          </header>
+        </Col>
       </Row>
       <Row>
-        <Event
-          title="Bike Ride"
-          description="Nice ride along the coast"
-          start={new Date()}
-          end={new Date()}
-          url="https://www.google.com"
-          email="hello@world.com"
-        />
+        <Col md={{ span: 6, offset: 3 }}>
+          <Event
+            title="Bike Ride"
+            description="Nice ride along the coast"
+            start={new Date()}
+            end={new Date()}
+            url="https://www.google.com"
+            email="hello@world.com"
+          />
+        </Col>
       </Row>
     </Container>
   );
